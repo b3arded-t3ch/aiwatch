0d9bfaf130aaaf054efbd121d65aee99961e4bf9
Tajudeen riliwan <dekunle4dreal1@gmail.com>
2024-09-12 15:31:37 +0100

    Added two new document from OpenAI timeline

--- sql/organization_docs/openai-org-docs.sql
+++ sql/organization_docs/openai-org-docs.sql
@@ -88,7 +88,7 @@ insert into organization_documents(url, title, publication_date, modified_date, 
 	'Jan Leike', /* affected_people */
 	'Successful hire', /* document_scope */
 	'AI Safety', /* cause_area */
-	'In a tweet, Jan Leike announces joining OpenAI to lead the alignment effort. Jan Leike, a prominent AI alignment researcher, announced joining OpenAI to lead their alignment effort. He expressed excitement about OpenAI''s work on reward modeling and aligning GPT-3 using human preferences. Leike''s hiring indicates OpenAI''s commitment to AI alignment research at that time. However, in a 2024 tweet, Leike revealed he joined OpenAI as he had thought it would be a better place to do research. But, due to long-standing disagreements with leadership about core priorities, he left OpenAI. This trajectory suggests potential shifts in OpenAI''s organizational focus or approach to alignment research over time, and highlights potential tensions between researchers'' goals and company priorities.' /* notes */
+	'In a tweet, Jan Leike announces joining OpenAI to lead the alignment effort. Jan Leike, a prominent AI alignment researcher, announces his joining OpenAI to lead their alignment effort. In comment, he states that his reason for joining OpenAI is that he loves OpenAI''s work on reward modeling and aligning GPT-3 using human preferences, and that he looks forward to building on it. However, in a 2024 tweet, Leike would reveal he joined OpenAI as he had thought it would be a better place to do research.' /* notes */
 )
 ,(
 	'https://openai.com/index/discovering-the-minutiae-of-backend-systems/', /* url */
@@ -102,5 +102,32 @@ insert into organization_documents(url, title, publication_date, modified_date, 
 	'Job experience', /* document_scope */
 	'AI', /* cause_area */
 	'This article contains an interview with Christian Gibson, an engineer on the supercomputing team of OpenAI, discussing how he got into programming as well as his work at OpenAI. In the interview, he discusses that his work focuses on solving problems that relate to Exploratory AI workflows by preempting research needs before they block progress and identifying bottlenecks as well as implementing workarounds as quickly as possible. Gibson''s account of working on supercomputing at OpenAI provides valuable insights into the organization''s operations and culture. According to Gibson, OpenAI operates at an unprecedented scale, utilizing billion-dollar supercomputers and encountering technical challenges that push the boundaries of existing hardware capabilities. He mentions that the employees actively work to save days of compute time by relentlessly focusing on performance optimization to enable them match the cutting-edge technology. The work environment is characterized by a mix of coding, problem-solving, and collaboration, with employees having a clear understanding of how their work impacts specific teams and projects. According to him, employees also have the opportunity to tackle unique technical challenges in high-performance computing which significantly contributes to employee motivation.' /* notes */
+)
+,(
+	'https://abcnews.go.com/Technology/openai-ceo-sam-altman-ai-reshape-society-acknowledges/story?id=97897122', /* url */
+	'OpenAI CEO Sam Altman says AI will reshape society, acknowledges risks: ''A little bit scared of this''', /* title */
+	'2023-03-16', /* publication_date */
+	NULL, /* modified_date */
+	'Victor Ordonez , Taylor Dunn, and Eric Noll', /* author */
+	'abc NEWS', /* publisher */
+	'OpenAI', /* affected_organizations */
+	'Sam Altman', /* affected_people */
+	'General discussion of organizational practices', /* document_scope */
+	'AI Safety', /* cause_area */
+	'A few months after the release of GPT-4, ABC News'' chief business, technology and economics correspondent Rebecca Jarvis interview Sam Altman about the rollout of GPT-4. In the interview, Sam Altman expresses his concerns about potential misuse of the AI technology; however, he maintains that OpenAI is in regular contact with government officials as the organization needs both regulators and society to prevent the potential negative consequences the technology could have on human.' /* notes */
+)
+,(
+	'https://time.com/6288245/openai-eu-lobbying-ai-act/',  /* url */
+	'Exclusive: OpenAI Lobbied the E.U. to Water Down AI Regulation',  /* title */
+	'2023-06-20', /* publication_date */
+	NULL, /* modified_date */
+	'Billy Perrigo', /* author */
+	'TIME', /* publisher */
+	'OpenAI', /* affected_organizations */ 
+	'Sam Altman', /* affected_people */
+	'Third-party commentary on organization', /* document_scope */
+	'AI Safety', /* cause_area */
+	'The author of this article claims that OpenAI manages to influence the E.U.''s AI Act. He argues that although Sam Altman has been the major preacher for global AI regulation, he lobbies, behind the scenes, for significant elements of the legislation to be in the favour of OpenAI. According to him, "OpenAI’s lobbying effort appears to have been a success: the final draft of the Act approved by E.U. lawmakers did not contain wording present in earlier drafts suggesting that general purpose AI systems should be considered inherently high risk". Meanwhile, the author claims that "in 2022, OpenAI repeatedly argued to European officials that the forthcoming AI Act should not consider its general purpose AI systems—including GPT-3, the precursor to ChatGPT, and the image generator Dall-E 2—to be high risk,"' /* notes */
 );
+
 
